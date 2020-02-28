2fa86023dd0183ae9bc37fe38f3b355ed6493bcb
Lautaro Jayat <lautaro.jayat@gmail.com>
2020-02-27 22:32:16 -0300

    Added a post install message

---
+++ post_install.js
@@ -0,0 +1,3 @@
+let FgGreen = "\x1b[32m"
+
+console.log('\n',FgGreen,'Thank you for downloading the Quick Twitter Bot!\nPlease, if you liked this repo, dont forget to give me a star in Github! :) :) :)\nhttps://github.com/LautaroJayat/quick-twitter-bot', Reset, '\n')
