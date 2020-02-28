57eb04a0cd066e64b1395d8acbed7ac525af1dca
Lautaro Jayat <lautaro.jayat@gmail.com>
2020-02-27 22:57:22 -0300

    Fixed post install message

--- post_install.js
+++ post_install.js
@@ -1,3 +1,16 @@
 let FgGreen = "\x1b[32m"
+let Reset = "\x1b[0m"
+let FgYellow = "\x1b[33m"
+console.log('\n')
+console.log('             °u°             °v°             °u°             °v°')
+console.log('            ~~|~~           ~~|~~           ~~|~~           ~~|~~')
+console.log('             / \\             / \\             / \\             / \\')
+console.log('---------------------------------------------------------------------------------------------')
+console.log(FgGreen, '||       Thank you for downloading the Quick Twitter Bot!                                ||', Reset)
+console.log("\x1b[35m", '||       Please, if you liked this repo, dont forget to give me a star in Github!        ||', Reset)
+console.log(FgYellow, '||       https://github.com/LautaroJayat/twitter_bot                                     ||', Reset)
+console.log("\x1b[36m", '||       :) :) :) ;) ;) :) :) :) ;) (L)                                                  ||', Reset)
+console.log('---------------------------------------------------------------------------------------------')
+console.log('\n')
 
-console.log('\n',FgGreen,'Thank you for downloading the Quick Twitter Bot!\nPlease, if you liked this repo, dont forget to give me a star in Github! :) :) :)\nhttps://github.com/LautaroJayat/quick-twitter-bot', Reset, '\n')
+
